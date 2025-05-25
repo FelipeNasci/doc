8e2a3b4cba84f6142db1adc72dfa79c66974dda6
felipenasci <dfnascimento@live.com>
2025-05-25 12:34:32 +0000

    change deployment branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "felipenasci", // Usually your GitHub org/user name.
   projectName: "doc", // Usually your repo name.
-  deploymentBranch: "main", // Usually your repo name.
+  deploymentBranch: "deployment", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
